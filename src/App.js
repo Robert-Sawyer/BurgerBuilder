c251d74597599c27c1f711c81ab13b65117c645d
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-04-01 19:11:14 +0200

    Converted App and Layout into React Hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,61 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, Suspense} from 'react';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
+const Checkout = React.lazy(() => {
+    return import('./containers/Checkout/Checkout');
+});
+
+const Orders = React.lazy(() => {
+    return import('./containers/Orders/Orders');
+});
+
+const Auth = React.lazy(() => {
+    return import('./containers/Auth/Auth');
+});
+
 const app = props => {
 
-  useEffect(() => {
-      props.onTryAutoSignup();
-  }, []);
+    useEffect(() => {
+        props.onTryAutoSignup();
+    }, []);
 
 //JAKO DODATKOWE ULEPSZENIE MOŻNA DODAĆ JESZCZE LENIWE ŁADOWANIE IMPORTÓW POPRZEZ ASYNCCOMPONENT - PRZYKŁAD W REACT-START-APP
     let routes = (
         <Switch>
-           <Route path="/auth" component={Auth}/>
-           <Route path="/" exact component={BurgerBuilder}/>
-           <Redirect to="/" />
+            <Route path="/auth" render={() => <Auth/>}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+            <Redirect to="/"/>
         </Switch>
     );
 
     if (props.isAuthenticated) {
         routes = (
             <Switch>
-               <Route path="/checkout" component={Checkout}/>
-               <Route path="/orders" component={Orders}/>
-               <Route path="/logout" component={Logout}/>
-			   <Route path="/auth" component={Auth}/>
-               <Route path="/" exact component={BurgerBuilder}/>
-               <Redirect to="/" />
+                <Route path="/checkout" render={() => <Checkout/>}/>
+                <Route path="/orders" render={() => <Orders/>}/>
+                <Route path="/logout" component={Logout}/>
+                <Route path="/auth" render={() => <Auth/>}/>
+                <Route path="/" exact component={BurgerBuilder}/>
+                <Redirect to="/"/>
             </Switch>
         );
     }
 
     return (
-      <div>
-        <Layout>
-            {routes}
-        </Layout>
-      </div>
+        <div>
+            <Layout>
+                {/*zastąp potem paragraf Spinnerem*/}
+                <Suspense fallback={<p>Loading...</p>}>
+                    {routes}
+                </Suspense>
+            </Layout>
+        </div>
     );
 }
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,53 +1,45 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Aux from '../AuxComponent/AuxComponent';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import {connect} from 'react-redux';
 
-class Layout extends Component {
+const layout = props => {
 
-    state = {
-        showSideDrawer: false
+    const [visibleSideDrawer, setVisibleSideDrawer] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setVisibleSideDrawer(false);
     };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    };
+    const sideDrawerToggleHandler = () => {
+        setVisibleSideDrawer(!visibleSideDrawer);
+        };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer}
-        });
-        //W taki sposób (poprzez this.state) lepiej tego nie robić, lepszy spopsób powyżej.
-        // this.setState({showSideDrawer: !this.state.showSideDrawer});
-    };
-
-    render() {
-        return (
+    return (
             <Aux>
                 {/*Jesli w {} jest this.jakaśMetoda to gdyby była z nawiasami na końcu była by wykonywana (executed) ale */}
                 {/*zawsze należy używać REFERENCJI do metody w klasie*/}
                 <Toolbar
-                    isAuth={this.props.isAuthenticated}
-                    drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                    isAuth={props.isAuthenticated}
+                    drawerToggleClicked={sideDrawerToggleHandler}/>
                 <SideDrawer
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler}/>
+                    isAuth={props.isAuthenticated}
+                    open={visibleSideDrawer}
+                    closed={sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Aux>
         )
-    }
 }
 
 const mapStateToProps = state => {
     return {
-    //jeśli token jest w state wtedy wiadomo, że user jest zalogowany czyli ma autoryzację
+        //jeśli token jest w state wtedy wiadomo, że user jest zalogowany czyli ma autoryzację
         isAuthenticated: state.auth.token !== null
     };
 };
 
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(layout);
